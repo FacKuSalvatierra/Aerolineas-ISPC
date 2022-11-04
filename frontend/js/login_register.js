302064d50cea3939a2b31a7301c9638be496cbbf
eliasgiannantonio <giannantonioelias@gmail.com>
2022-11-04 01:05:50 -0300

    Hice un re-diseño de los sitios web, con nuevos estilos, funciones y caracteristicas, utilizando Javascript, HTML y CSS. Agregué una barra de navegacion responsiva, reemplacé el carrusel anterior por uno más moderno y además responsivo. También utilicé bootstrap para ciertas secciones específicas, como el select de lugar de origen y lugar de destino, agregué tarjetas principales de destinos turísticos y sus respectivas imágenes llamativas. Cree la pagina login responsive, hice un re-diseño del modelo de contacto de Facu Morel, queda ajustar el responsive en ciertas partes de los sitios

---
+++ frontend/js/login_register.js
@@ -0,0 +1,28 @@
+//   Mostrar / ocultar contraseña
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        pwFields.forEach(pwField => {
+            if (pwField.type === "password") {
+                pwField.type = "text";
+
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace("uil-eye-slash", "uil-eye");
+                })
+            } else {
+                pwField.type = "password";
+
+                pwShowHide.forEach(icon => {
+                    icon.classList.replace("uil-eye", "uil-eye-slash");
+                })
+            }
+        })
+    })
+})
+
+// Registro e inicio de sesion
+signUp.addEventListener("click", () => {
+    container.classList.add("active");
+});
+login.addEventListener("click", () => {
+    container.classList.remove("active");
+});

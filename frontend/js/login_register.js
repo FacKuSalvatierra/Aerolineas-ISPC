5f5ca9d2ae8a4ef77211580d577df3ef7b724201
eliasgiannantonio <giannantonioelias@gmail.com>
2022-11-10 16:30:07 -0300

    Añadidas correcciones y funcionalidad Javascript en formulario de inicio de sesión / registro

--- frontend/js/login_register.js
+++ frontend/js/login_register.js
@@ -27,10 +27,49 @@ pwShowHide.forEach(eyeIcon => {
 })
 
 
-// Registro e inicio de sesion
+// Toggle Registro e inicio de sesion
 signUp.addEventListener("click", () => {
     container.classList.add("active");
 });
 login.addEventListener("click", () => {
     container.classList.remove("active");
 });
+
+//Validar form
+
+// Validar inicio sesion
+function validar_inicio(){
+    let email = document.getElementById("email_login").value
+    let pass = document.getElementById("password_login").value
+    if (email == ""){
+        alert("Debe ingresar su correo electrónico");
+    }
+    else if (pass == ""){
+        alert("Debe ingresar su contraseña");
+    }
+
+}
+
+// Validar registro
+function validar_registro(){
+    let name = document.getElementById("name_register").value
+    let email = document.getElementById("email_register").value
+    let pass = document.getElementById("password_register").value
+    let pass_conf = document.getElementById("password_register_conf").value
+    let terms = document.getElementById("term").value
+    if (name == ""){
+        alert("Debe ingresar su nombre");
+    }
+    else if (email == ""){
+        alert("Debe ingresar su correo electrónico");
+    }
+    else if (pass == ""){
+        alert("Debe ingresar su contraseña");
+    }
+    else if (pass_conf == ""){
+        alert("Debe confirmar su contraseña");
+    }
+    else if (terms == false){
+        alert("Debe aceptar los términos y condiciones")
+    }
+}

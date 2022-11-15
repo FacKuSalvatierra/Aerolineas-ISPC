591448c3ecd4b0659021b5b599e5bc67430ce7e3
Facundo <98361543+FacKuSalvatierra@users.noreply.github.com>
2022-11-14 23:11:43 -0300

    se arreglo el modo oscuro

---
+++ frontend/js/modo-oscuro.js
@@ -0,0 +1,20 @@
+let switchButtom = document.getElementById("switch");
+let body = document.body;
+switchButtom.addEventListener("click", function () {
+  let val = body.classList.toggle("dark");
+  switchButtom.classList.toggle("active");
+  localStorage.setItem("switch", val);
+
+});
+
+let valor=localStorage.getItem("switch")
+if (valor=="true") {
+    body.classList.add("dark")
+} else {
+    body.classList.remove("dark")
+}
+
+
+
+  
+
